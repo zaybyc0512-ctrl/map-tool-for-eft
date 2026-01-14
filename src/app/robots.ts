ae646b027e8e1a237f729a26b3558b04d413a701
zaybyc0512-ctrl <zaybyc0512@gmail.com>
2026-01-14 11:29:21 +0900

    robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
+        sitemap: 'https://map-tool-for-eft.com/sitemap.xml',
+    }
+}
+// ビルドエラー回避のために静的生成を指定
+export const dynamic = 'force-static'
